d5ca1f94fcae70a4e3d82b62da0902e64a0f3f72
hieumthe1638431 <hieumthe163841@fpt.edu.vn>
2023-12-07 14:34:11 +0700

    Add project files.

---
+++ RazorWeb/Models/MyBlogContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorWeb.Models
+{
+    public class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> options) : base(options) 
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Article> articles { get; set; }
+
+    }
+}
